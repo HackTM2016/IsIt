6c1abc15b209471b46ab45c8bcacb704f62fa489
Timo Bejan <timo.bejan.ext@iqser.com>
2016-05-22 03:20:39 +0300

    added cors

--- src/main/java/is/it/good/rest/QuestionRestController.java
+++ src/main/java/is/it/good/rest/QuestionRestController.java
@@ -4,6 +4,7 @@ import is.it.good.model.QuestionModel;
 import is.it.good.model.ResponseModel;
 import is.it.good.service.loader.DLoader;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,6 +16,7 @@ public class QuestionRestController {
 	@Autowired
 	private DLoader dLoader;
 
+	@CrossOrigin(origins = { "http://localhost:8100", "http://localhost" })
 	@RequestMapping("/api/question")
 	@ResponseBody
 	public ResponseModel getResponse(@RequestParam String question,
