ec71db986ffda44708f8846c296ecd61faf0aace
Timo Bejan <timo.bejan.ext@iqser.com>
2016-05-22 03:33:44 +0300

    fixed positive force

--- src/main/java/is/it/good/model/CategoryAndRatingModel.java
+++ src/main/java/is/it/good/model/CategoryAndRatingModel.java
@@ -2,10 +2,18 @@ package is.it.good.model;
 
 public class CategoryAndRatingModel {
 
-	public final static CategoryAndRatingModel SUPER_POSITIVE = new CategoryAndRatingModel();
+	public final static CategoryAndRatingModel SUPER_POSITIVE = new CategoryAndRatingModel(Category.POSITIVE,1.0d);
 
 	private Category category;
 	private double rating;
+
+	public CategoryAndRatingModel() {
+	}
+
+	public CategoryAndRatingModel(Category category, double rating) {
+		this.category = category;
+		this.rating = rating;
+	}
 
 	public Category getCategory() {
 		return category;
